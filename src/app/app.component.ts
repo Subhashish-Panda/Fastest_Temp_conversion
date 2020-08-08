437a3b28635f78f996520109f186bb0bd68a99ac
Subhashish Panda <41316390+Subhashish-Panda@users.noreply.github.com>
2020-08-08 23:45:42 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent  {
    if(this.c!=null){
    this.f=((this.c*9)/5)+32;
    this.f=Math.round(this.f*100)/100;
-   this.k=273.15+this.c;
+   this.k=Math.round((273.15+this.c)*100)/100;
    }
    else{
      this.f=undefined;
@@ -27,7 +27,7 @@ export class AppComponent  {
    if(this.f!=null){
    this.c=((this.f-32)*5)/9;
    this.c=Math.round(this.c*100)/100;
-   this.k=273.15+this.c;
+   this.k=Math.round((273.15+this.c)*100)/100;
  }
  else{
    this.c=undefined;
